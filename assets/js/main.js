583a4f2c6c1fc5408fb5dbdffb381d0525dd0032
Костадинов Ѓорги <gjorgi.kostadinov@students.finki.ukim.mk>
2025-04-03 21:37:44 +0200

    ALL DONE FINAL 1.0.0 XG27

--- assets/js/main.js
+++ assets/js/main.js
@@ -174,33 +174,6 @@ Thank you - https://bootstrapmade.com/
   /**
    * Menu isotope and filter
    */
-
-
-  // window.addEventListener('load', () => {
-  //   let menuContainer = select('.menu-container');
-  //   if (menuContainer) {
-  //     // Initialize Isotope
-  //     let menuIsotope = new Isotope(menuContainer, {
-  //       itemSelector: '.menu-item',
-  //       layoutMode: 'fitRows'
-  //     });
-
-  //     let menuFilters = select('#menu-flters li', true);
-
-  //     on('click', '#menu-flters li', function(e) {
-  //       e.preventDefault();
-  //       menuFilters.forEach(function(el) {
-  //         el.classList.remove('filter-active');
-  //       });
-  //       this.classList.add('filter-active');
-
-  //       menuIsotope.arrange({
-  //         filter: this.getAttribute('data-filter')
-  //       });
-
-  //     }, true);
-  //   }
-  // });
   window.addEventListener('load', () => {
     let menuContainer = select('.menu-container');
     if (menuContainer) {
